2014fae3ec335a57fbb798dbb82e89b9b3f7ea4a
1Shubham7 <shubhammahar1306@gmail.com>
2023-06-26 19:36:01 +0530

    added api values to add.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const articles = [
+  {
+    id: 1,
+    title: "Nature walk in the park",
+    description: "Visit the park with my friends",
+    completed: true
+  },
 
+  {
+    id: 2,
+    title: "Visit",
+    description: "Got to my aunt's place",
+    completed: true
+  },
+
+  {
+    id: 3,
+    title: "Write",
+    description: "Do an article about anthropology",
+    completed: true
+  },
+];
+
+class App extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {articles};
+    };
+
+    render() {
+      return (
+        <main className="content">
+        <div className="row">
+          <div className="col-md-6 col-sm-10 mx-auto p-0">
+            <div className="card p-3">
+              <ul className="list-group list-group-flush">
+              {this.state.articles.map(item => (
+              <div>
+                <h1>{item.title}</h1>
+                <span>{item.description}</span>
+              </div>
+              ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </main>
+      )
+    }
+  }
+  
 export default App;
