43ebbd4c00a0252cebb61b1130309e9f082bb0ef
1Shubham7 <shubhammahar1306@gmail.com>
2023-06-26 20:36:15 +0530

    added npm library + updated app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
 import React, { Component } from "react"
+import Modal from "./components/Modal"; 
+import axios from "axios";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
+    state = {
       viewCompleted: false,
       activeItem: {
         title: "",
@@ -13,8 +11,7 @@ class App extends Component {
         completed: false
       },
       todoList: []
-      };
-  }
+    };
 
     async componentDidMount() {
       try {
@@ -27,6 +24,53 @@ class App extends Component {
         console.log(e);
     }
     }
+
+    toggle = () => {
+      this.setState({ modal: !this.state.modal });
+    };
+  
+    //Responsible for saving the task
+    handleSubmit = item => {
+      this.toggle();
+      if (item.id) {
+        axios
+          .put(`http://localhost:8000/api/todos/${item.id}/`, item)
+        return;  
+      }
+      axios
+        .post("http://localhost:8000/api/todos/", item)
+    };
+
+    createItem = () => {
+      const item = {title: "", description: "", completed: false };
+      this.setState({ activeItem: item, modal: !this.state.modal });
+    };
+
+    displayCompleted = status => {
+      if (status) {
+        return this.setState({ viewCompleted: true});
+      }
+      return this.setState({ viewCompleted: false});
+    };
+    renderTabList = () => {
+      return (
+        <div className="my-5 tab-list">
+          <button 
+            onClick={() => this.displayCompleted(true)}
+            className={this.state.viewCompleted ? "active" : ""}
+          >
+            Complete
+          </button>
+          <button 
+            onClick={() => this.displayCompleted(false)}
+            className={this.state.viewCompleted ? "" : "active"}
+          >
+            Incomplete
+          </button>
+        </div>  
+      );
+    };
+
     renderItems = () => {
       const { viewCompleted } = this.state;
       const newItems = this.state.todoList.filter(
@@ -52,15 +96,27 @@ class App extends Component {
     render() {
       return (
         <main className="content">
+        <h1 className="text-white text-uppercase text-center my-4">Todo App</h1>
         <div className="row">
           <div className="col-md-6 col-sm-10 mx-auto p-0">
             <div className="card p-3">
+              <div className="">
+                <button onClick={this.createItem} className="btn btn-success">Add Task</button>
+              </div>
+              {this.renderTabList()}
               <ul className="list-group list-group-flush">
                 {this.renderItems()}
               </ul>
             </div>
           </div>
         </div>
+        {this.state.modal ? (
+          <Modal
+            activeItem={this.state.activeItem}
+            toggle={this.toggle}
+            onSave={this.handleSubmit}
+          />
+        ): null}
       </main>
       )
     }
