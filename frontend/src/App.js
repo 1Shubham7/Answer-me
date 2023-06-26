5ced561baab01107fb4ea07b3aa3dd2c00d145a5
1Shubham7 <shubhammahar1306@gmail.com>
2023-06-26 20:33:20 +0530

    changed the add.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,33 +2,51 @@ import logo from './logo.svg';
 import './App.css';
 import React, { Component } from "react"
 
-const articles = [
-  {
-    id: 1,
-    title: "Nature walk in the park",
-    description: "Visit the park with my friends",
-    completed: true
-  },
-
-  {
-    id: 2,
-    title: "Visit",
-    description: "Got to my aunt's place",
-    completed: true
-  },
-
-  {
-    id: 3,
-    title: "Write",
-    description: "Do an article about anthropology",
-    completed: true
-  },
-];
-
 class App extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {articles};
+  constructor(props) {
+    super(props);
+    this.state = {
+      viewCompleted: false,
+      activeItem: {
+        title: "",
+        description: "",
+        completed: false
+      },
+      todoList: []
+      };
+  }
+
+    async componentDidMount() {
+      try {
+        const res = await fetch('http://localhost:8000/api/todos/');
+        const todoList = await res.json();
+        this.setState({
+          todoList
+        });
+      } catch (e) {
+        console.log(e);
+    }
+    }
+    renderItems = () => {
+      const { viewCompleted } = this.state;
+      const newItems = this.state.todoList.filter(
+        item => item.completed === viewCompleted
+      );
+      return newItems.map(item => (
+        <li 
+          key={item.id}
+          className="list-group-item d-flex justify-content-between align-items-center"
+        >
+          <span 
+            className={`todo-title mr-2 ${
+              this.state.viewCompleted ? "completed-todo" : ""
+            }`}
+            title={item.description}
+            >
+              {item.title}
+            </span>
+        </li>
+      ));
     };
 
     render() {
@@ -38,12 +56,7 @@ class App extends Component {
           <div className="col-md-6 col-sm-10 mx-auto p-0">
             <div className="card p-3">
               <ul className="list-group list-group-flush">
-              {this.state.articles.map(item => (
-              <div>
-                <h1>{item.title}</h1>
-                <span>{item.description}</span>
-              </div>
-              ))}
+                {this.renderItems()}
               </ul>
             </div>
           </div>
